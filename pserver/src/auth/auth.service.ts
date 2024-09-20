a42fb5b7ee208431ed2019a5e0e4db5dd472f080
Alex P <mycop14@mail.ru>
2024-09-20 22:20:24 +0300

    fix cookie domain

--- pserver/src/auth/auth.service.ts
+++ pserver/src/auth/auth.service.ts
@@ -106,7 +106,7 @@ export class AuthService {
 
     res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
       httpOnly: true,
-      // domain: 'localhost',
+      domain: this.configService.get('DOMAIN'),
       secure: true,
       sameSite: 'none',
       expires: expiresIn,
@@ -116,7 +116,7 @@ export class AuthService {
   removeRefreshTokenToResponse(res: Response) {
     res.cookie(this.REFRESH_TOKEN_NAME, '', {
       httpOnly: true,
-      // domain: 'localhost',
+      domain: this.configService.get('DOMAIN'),
       secure: true,
       sameSite: 'none',
       expires: new Date(0),

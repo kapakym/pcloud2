fe04fc4c69e0e7cd6eaac17c7ee3c463d5c3c968
Alex P <mycop14@mail.ru>
2024-10-12 00:03:24 +0300

    feat: add proxy on to server

--- pclient/next.config.mjs
+++ pclient/next.config.mjs
@@ -1,41 +1,17 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {}
-// const nextConfig = {
-// 	async rewrites() {
-// 		return [
-// 			{
-// 				source: '/api/:path*', // Прокси все запросы, начинающиеся с /api
-// 				destination:
-// 					process.env.NODE_ENV === 'production'
-// 						? 'http://server:5555/api/:path*'
-// 						: 'http://localhost:5555/api/:path*' // Прокси запросы на NestJS контейнер,
-// 			}
-// 			// {
-// 			// 	source: '/tasks', // Прокси все запросы, начинающиеся с /api
-// 			// 	destination:
-// 			// 		process.env.NODE_ENV === 'production'
-// 			// 			? 'http://server:5555/tasks'
-// 			// 			: 'http://localhost:5555/tasks' // Прокси запросы на NestJS контейнер,
-// 			// }
-// 		]
-// 	},
-// 	// webpack: (config, { isServer }) => {
-// 	// 	// if (!isServer) {
-// 	// 	// Прокси WebSocket соединений для клиентской части
-// 	// 	config.devServer = {
-// 	// 		...config.devServer,
-// 	// 		proxy: {
-// 	// 			'/tasks': {
-// 	// 				target: 'http://localhost:5555', // Адрес вашего NestJS сервера
-// 	// 				ws: true, // Включаем поддержку WebSocket
-// 	// 				changeOrigin: true
-// 	// 			}
-// 	// 		}
-// 	// 	}
-// 	// 	// }
-// 	// 	return config
-// 	// }
-// }
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*', // Прокси все запросы, начинающиеся с /api
+				destination:
+					process.env.NODE_ENV === 'production'
+						? 'http://server:5555/api/:path*'
+						: 'http://localhost:5555/api/:path*' // Прокси запросы на NestJS контейнер,
+			}
+		]
+	}
+}
 
 export default nextConfig
